01ae7698d7611b557ec4ad220d395e3baf2d463a
Gassner, Sonja <Sonja.Gassner@zuehlke.com>
2017-10-16 15:57:45 +0200

    examples finally working

--- tryouts_R/deploy_tryout.R
+++ tryouts_R/deploy_tryout.R
@@ -12,9 +12,20 @@ f.paste <- function(text1, text2){
     return(paste(text1, text2))
 }
 
+## the following is what we need:
+
+#* @post /names
+f.test <- function(req){
+    json <- req$postBody # access the json directly
+    list <- fromJSON(json)
+    df <- as.data.frame(list)
+    res <- apply(df, 1, mean)
+    return(paste("mean of a and b is:", res ))   #return(unbox(res)) 
+}
+
 # funktionieren beide von Postman aus, und neu auch von R direkt
 
-#write the following in (new) R Session to generate Web server
+# write the following in (new) R Session to generate Web server
 # library(plumber)
 # r <- plumb("deploy_tryout.R")
 # r$run(port=8000)
--- tryouts_R/post_call_to_localhost_tryout.R
+++ tryouts_R/post_call_to_localhost_tryout.R
@@ -2,35 +2,66 @@ library(jsonlite)
 library(df2json)
 library(httr)
 
-list <- list(a = 4, b = 6)
+## Conversion data frame to json to data frame: ####
 
-# the following works:
+# nice data frame
+(df <- data.frame(name=c("a", "b", "c"), y=c(10, 20, 4), show=c(TRUE, FALSE, TRUE)))
+js <- toJSON(df)
+cat(js)
+(df2 <- fromJSON(js))
+as.data.frame(df2)
 
-### send data as list:
-res <- POST(url = "localhost:8000/test", body = list, encode = "json", verbose(), accept_json())
-content(res)
+## Achtung: das funktioniert nicht richtig bei NAs und -Inf -> abfangen!!
+(df <- data.frame(name=c("a", "b", "c"), x=c(NA, 2 ,3), y=c(10, 20, -Inf), show=c(TRUE, FALSE, TRUE)))
+js <- toJSON(df)
+cat(js)
+(df2 <- fromJSON(js))
+as.data.frame(df2)
 
-res <- POST(url = "localhost:8000/paste", body = list(text1 = "hello ", text2 = "world"), encode = "json", verbose())
-stop_for_status(res)
-content(res)
 
+
+## POST call to localhost: ####
 
 ### send data as json:
+
+# from data frame:
+df <- data.frame(a = 5, b = 6)  
+js <- toJSON(df)
+cat(js)
+res <- POST(url = "localhost:8000/names", body = js, verbose(), accept_json())
+content(res)
+
+
+
+
+
+
+
+
+
 # from list
 js <- toJSON(list) 
 cat(js)
 res <- POST(url = "localhost:8000/test", body = js, verbose(), accept_json())
 content(res)
 
-# from data frame:
-df <- data.frame(a = 4, b = 6)  
-# js <- toJSON(df) # not working
+# need to convert the data frame to a list first!
+# lst <- as.list(df)
+# lst
+# js <- toJSON(lst)
 # cat(js)
 # res <- POST(url = "localhost:8000/test", body = js, verbose(), accept_json())
+# content(res)
 
-# need to convert the data frame to a list first!
-lst <- as.list(df)
-lst
-js <- toJSON(lst)
-res <- POST(url = "localhost:8000/test", body = js, verbose(), accept_json())
+
+
+
+
+### send data as list:
+list <- list(a = 4, b = 6)
+res <- POST(url = "localhost:8000/test", body = list, encode = "json", verbose(), accept_json())
+content(res)
+
+res <- POST(url = "localhost:8000/paste", body = list(text1 = "hello ", text2 = "world"), encode = "json", verbose())
+stop_for_status(res)
 content(res)
