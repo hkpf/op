a4f30f50b828977680fd89d70e70865f05b5a183
Gassner, Sonja <Sonja.Gassner@zuehlke.com>
2017-10-19 09:32:06 +0200

    Testing etwas generalisiert, so dass jede Beobachtung (obs) aus test-dataframen gewählt werden kann

--- post_request_to_RESTApi.R
+++ post_request_to_RESTApi.R
@@ -17,8 +17,10 @@ post.df.to.server <- function(data.frame, url){
 
 
 # Testing
+obs <- 8
 d.test <- readRDS("mnist_dataframes/mnist_test_dataframe.rds")
-post.df.to.server(d.test[1,-785], "localhost:8000/predict")
+post.df.to.server(d.test[obs,-785], "localhost:8000/predict")
+d.test[obs,785]
 
 # NAs
 post.df.to.server((is.na(d.test[1,-785]) <- 1), "localhost:8000/predict")
