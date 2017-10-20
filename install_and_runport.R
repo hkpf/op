64944b07f60790c67b511cb2b0d4af6766568e34
Gassner, Sonja <Sonja.Gassner@zuehlke.com>
2017-10-20 16:53:23 +0200

    file ready for vm, packages are installed there already, so this part is now in comments

--- install_and_runport.R
+++ install_and_runport.R
@@ -1,36 +1,33 @@
+# Run this script on SERVER
 
-# install packages from CRAN
-install.packages("plumber")
-install.packages("randomForest")
-install.packages("jsonlite")
-install.packages("devtools")
 
+# # install packages from CRAN
+# install.packages("plumber")
+# install.packages("randomForest")
+# install.packages("jsonlite")
+# install.packages("devtools")
+# 
+# # install our packages from local repo on vm
+# install.packages("C:/Users/soga/Desktop/op/digiterEmpty_0.1.0.tar.gz", repos = NULL, type = "source")
+# install.packages("C:/Users/soga/Desktop/op/digiterEmpty_0.1.0.tar.gz", repos = NULL, type = "source")
+# install.packages("C:/Users/soga/Desktop/op/digiterEmpty_0.1.0.tar.gz", repos = NULL, type = "source")
+
+# not needed anymore: install our own packages from github repo
+# to load models and predict functions from within the packages
+# install_github("hkpf/op/digiterLarge")
+# install_github("hkpf/op/digiterSmall")
+# install_github("hkpf/op/digiterEmpty")
+
+# load packages
 library(plumber)
 library(randomForest)
 library(devtools)
 library(jsonlite)
-
-# install our own packages from github repo
-# to load models and predict functions from within the packages
-install_github("hkpf/op/digiterLarge")
-install_github("hkpf/op/digiterSmall")
-install_github("hkpf/op/digiterEmpty")
-
-#install("C:/Users/soga/Desktop/op")
-
-
-# # load packages
-# library(plumber, lib.loc="C:/Program Files/R/R-3.4.2/library")
-# library(jsonlite, lib.loc="C:/Program Files/R/R-3.4.2/library")
-# library(randomForest, lib.loc="C:/Program Files/R/R-3.4.2/library")
-# library(devtools, lib.loc="C:/Program Files/R/R-3.4.2/library")
 library(digiterEmpty)
 library(digiterSmall)
 library(digiterLarge)
 
-
-setwd("C:/Users/soga/Desktop/op")
+#setwd("C:/Users/soga/Desktop/op")
 r <- plumb("deploy_rf_pkg.R")
-r <- plumb("deploy_randomForest.R")
 r$run(port=80)
 
