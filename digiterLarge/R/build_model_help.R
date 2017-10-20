6d6d133e3ca3376654f3bfd304b2f22dcca0eccd
Pohl, Veronica <Veronica.Pohl@zuehlke.com>
2017-10-20 11:24:54 +0200

    HowTo_Create_RPackage.docx hochgeladen. Ist aber moch unvollstaendig! und R packagesohne mein directory versehen

--- digiterLarge/R/build_model_help.R
+++
@@ -1,4 +0,0 @@
-library(devtools)
-
-modellarge <- readRDS(file = "C:/Users/vepo/Documents/GitRepo/operationalisierung/models/model_rf_60000.rds")
-devtools::use_data(modellarge, internal = TRUE, overwrite = TRUE) #save modellarge in sysdata.rda
--- digiterSmall/modelbuilder/build_model_help.R
+++ digiterSmall/modelbuilder/build_model_help.R
@@ -1,4 +1,4 @@
 library(devtools)
 
-modelsmall <- readRDS(file = "C:/Users/vepo/Documents/GitRepo/operationalisierung/models/model_rf_1000.rds")
-devtools::use_data(modelsmall,modelsmall, internal = TRUE, overwrite = TRUE)
+#modelsmall <- readRDS(file = "C:/Users/vepo/Documents/GitRepo/operationalisierung/models/model_rf_1000.rds")
+#devtools::use_data(modelsmall,modelsmall, internal = TRUE, overwrite = TRUE)
