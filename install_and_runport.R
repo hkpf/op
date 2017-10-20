c52c816181e7b26672b6d8b4b0ddea0207a23ac9
Gassner, Sonja <Sonja.Gassner@zuehlke.com>
2017-10-20 15:31:47 +0200

    changed library commands

--- install_and_runport.R
+++ install_and_runport.R
@@ -5,7 +5,10 @@ install.packages("randomForest")
 install.packages("jsonlite")
 install.packages("devtools")
 
+library(plumber)
+library(randomForest)
 library(devtools)
+library(jsonlite)
 
 # install our own packages from github repo
 # to load models and predict functions from within the packages
@@ -16,15 +19,18 @@ install_github("hkpf/op/digiterEmpty")
 #install("C:/Users/soga/Desktop/op")
 
 
-# load packages
-library(plumber, lib.loc="C:/Program Files/R/R-3.4.2/library")
-library(jsonlite, lib.loc="C:/Program Files/R/R-3.4.2/library")
-library(randomForest, lib.loc="C:/Program Files/R/R-3.4.2/library")
-library(digiterEmpty, lib.loc="C:/Program Files/R/R-3.4.2/library")
-library(digiterSmall, lib.loc="C:/Program Files/R/R-3.4.2/library")
-library(digiterLarge, lib.loc="C:/Program Files/R/R-3.4.2/library")
+# # load packages
+# library(plumber, lib.loc="C:/Program Files/R/R-3.4.2/library")
+# library(jsonlite, lib.loc="C:/Program Files/R/R-3.4.2/library")
+# library(randomForest, lib.loc="C:/Program Files/R/R-3.4.2/library")
+# library(devtools, lib.loc="C:/Program Files/R/R-3.4.2/library")
+library(digiterEmpty)
+library(digiterSmall)
+library(digiterLarge)
 
 
+setwd("C:/Users/soga/Desktop/op")
 r <- plumb("deploy_rf_pkg.R")
+r <- plumb("deploy_randomForest.R")
 r$run(port=80)
 
