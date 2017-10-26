0f87aca55f2a32e03ee6e41b3116ee76ae2d9c23
Gassner, Sonja <Sonja.Gassner@zuehlke.com>
2017-10-26 11:19:00 +0200

    removed devtools, not needed anymore since we don't install directly from github anymore; furthermore setwd for linux VM

--- install_and_runport.R
+++ install_and_runport.R
@@ -1,18 +1,22 @@
 # Run this script on SERVER
 
+# Windows VM: 
+# setwd("C:/Users/soga/Desktop/op")
+# Linux VM:
+setwd("/home/soga/op")
 
 # # install packages from CRAN
 install.packages("plumber")
 install.packages("randomForest")
 install.packages("jsonlite")
-# install.packages("devtools")
-# 
+
 # # install our packages from local repo on vm
 install.packages("digiterEmpty_0.1.0.tar.gz", repos = NULL, type = "source")
 install.packages("digiterEmpty_0.1.0.tar.gz", repos = NULL, type = "source")
 install.packages("digiterEmpty_0.1.0.tar.gz", repos = NULL, type = "source")
 
 # not needed anymore: install our own packages from github repo
+# install.packages("devtools")
 # to load models and predict functions from within the packages
 # install_github("hkpf/op/digiterLarge")
 # install_github("hkpf/op/digiterSmall")
@@ -27,7 +31,7 @@ library(digiterEmpty)
 library(digiterSmall)
 library(digiterLarge)
 
-#setwd("C:/Users/soga/Desktop/op")
+
 r <- plumb("deploy_rf_pkg.R")
 r$run(port=80)
 
